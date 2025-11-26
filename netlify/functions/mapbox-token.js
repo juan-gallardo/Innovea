9595668f6aebc265763a19083f5e833474c55d9a
Juan Gallardo <juan.enrique.gallardo.flores@gmail.com>
2025-11-26 19:50:50 -0300

    Incorporación Mapbox más descarga informe completo en la última página.

--- netlify/functions/mapbox-token.js
+++ netlify/functions/mapbox-token.js
@@ -1,17 +1,9 @@
 // netlify/functions/mapbox-token.js
 // Esta función serverless protege tu token de Mapbox
 
-exports.handler = async (event, context) => {
-  // El token se lee de las variables de entorno de Netlify
-  const token = process.env.MAPBOX_TOKEN;
-  
+exports.handler = async () => {
   return {
     statusCode: 200,
-    headers: {
-      'Content-Type': 'application/json',
-      // Permite solo tu dominio de Netlify
-      'Access-Control-Allow-Origin': '*',
-    },
-    body: JSON.stringify({ token })
+    body: JSON.stringify({ token: process.env.MAPBOX_TOKEN }),
   };
 };
